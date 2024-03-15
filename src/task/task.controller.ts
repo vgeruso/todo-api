8ae4c3b077d8ef4049178fe01f6cc2fac1767281
vgeruso <victor.geruso@gmail.com>
2024-03-15 13:17:35 -0300

    [ADD] task controller

---
+++ src/task/task.controller.ts
@@ -0,0 +1,39 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('task')
+@Controller('api/v1/task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Post()
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    return await this.taskService.create(createTaskDto);
+  }
+
+  @Get()
+  async findAll() {
+    return await this.taskService.findAll();
+  }
+
+  @Put('update')
+  async update(@Body() updateTaskDto: UpdateTaskDto) {
+    return await this.taskService.update(updateTaskDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return await this.taskService.remove(id);
+  }
+}
